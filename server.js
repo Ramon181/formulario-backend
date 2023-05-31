99ae2d85d127f4e642a62cff70802fda8f98beb6
Ramon181 <ramonantonio29970476@gmail.com>
2023-05-30 23:42:14 -0500

    Solicitud Post a la base de datos y get para que me de la lista

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const server = express();
+const port = 3001;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "form",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error al conectar a la base de datos:", err);
+  } else {
+    console.log("Conexión exitosa a la base de datos");
+  }
+});
+
+server.use(cors());
+server.use(express.json())
+
+server.post('/form', (req, res) => {
+    const { name, country } = req.body;
+    if (!name || !country) {
+      return res.status(400).json({ message: 'Faltan campos requeridos' });
+    }
+    const query = 'INSERT INTO formulario (name, country) VALUES (?, ?)';
+    db.query(query, [name, country], (err, result) => {
+      if (err) {
+        console.error('Error al guardar los datos del formulario:', err);
+        return res.status(500).json({ message: 'Error del servidor' });
+      }
+      return res.status(200).json({ message: 'Datos del formulario guardados correctamente' });
+    });
+  });
+
+  server.get('/form', (req, res) => {
+    const query = 'SELECT * FROM formulario';
+    db.query(query, (err, result) => {
+      if (err) {
+        console.error('Error al obtener el formulario:', err);
+        return res.status(500).json({ message: 'Error del servidor' });
+      }
+      return res.status(200).json(result);
+    });
+  });
+
+server.listen(port, () => {
+  console.log(`Servidor backend escuchando en el puerto ${port}`);
+});
